eb0f14badcd92a4132c9fa5b7b8fde6c202e4e51
Brendan Kowitz <brendan.kowitz@gmail.com>
2018-02-07 23:26:59 -0800

    Adds dependency classes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # smote-csharp
 Implements SMOTE for imbalanced datasets in C#
+
+Based on pseudocode from the whitepaper: https://arxiv.org/pdf/1106.1813.pdf
---
+++ src/MachineLearning/Smote/Proximity/Distance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ZeroProximity.MachineLearning.Proximity
+{
+    public static class Distance
+    {
+        /// <summary>
+        /// Calculates the Euclidean Distance Measure between two data points
+        /// </summary>
+        public static double EuclideanDistance(double[] x, double[] y)
+        {
+            int count;
+            double distance;
+            double sum = 0.0;
+
+            if (x.GetUpperBound(0) != y.GetUpperBound(0))
+            {
+                throw new ArgumentException("The number of elements in X must match the number of elements in Y");
+            }
+
+            count = x.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                sum = sum + Math.Pow(Math.Abs(x[i] - y[i]), 2);
+            }
+
+            distance = Math.Sqrt(sum);
+
+            return distance;
+        }
+    }
+}
